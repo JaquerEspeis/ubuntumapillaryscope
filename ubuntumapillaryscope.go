0283279367e10ddd1f56c273a7fadc9eb1609113
Leo Arias <yo@elopio.net>
2015-09-19 08:35:30 -0600

    Initial version.

---
+++ ubuntumapillaryscope.go
@@ -0,0 +1,88 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2015 JaquerEspeis
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+// Package main implements an Ubuntu scope for Mapillary street level photos.
+package main
+
+import (
+	"log"
+	"net/url"
+
+	"github.com/JaquerEspeis/mapillary"
+	mapillaryapi "github.com/JaquerEspeis/mapillary/v2"
+	"launchpad.net/go-unityscopes/v2"
+)
+
+const clientID = "WHJxWW40RWhyVGR6ajVEci1saHZNUTpiNmViMzg2MTQwMjkwZjhk"
+
+// UbuntuMapillaryScope is a scope that shows Mapillary street level photos.
+type UbuntuMapillaryScope struct {
+}
+
+type imgInfo struct {
+	*mapillaryapi.GetSearchImRandomSelect
+	ImageURL string
+}
+
+// SetScopeBase is defined just to conform with the interface.
+func (s *UbuntuMapillaryScope) SetScopeBase(base *scopes.ScopeBase) {
+}
+
+// Search pushes results to the scope.
+func (s *UbuntuMapillaryScope) Search(query *scopes.CannedQuery, metadata *scopes.SearchMetadata, reply *scopes.SearchReply, cancelled <-chan bool) error {
+	cat := reply.RegisterCategory("curated", "Curated Images", "", "")
+
+	result := scopes.NewCategorisedResult(cat)
+	info, err := s.getRandomCuratedImageInfo()
+	if err != nil {
+		return err
+	}
+	result.SetURI(info.ImageURL)
+	result.SetTitle(info.Location)
+	result.SetArt(info.ImageURL)
+	return reply.Push(result)
+}
+
+func (s *UbuntuMapillaryScope) getRandomCuratedImageInfo() (info imgInfo, err error) {
+	client := mapillaryapi.NewClient(clientID)
+	var response imgInfo
+	if err := client.Get("search/im/randomselected", url.Values{}, &response); err != nil {
+		return response, err
+	}
+	response.ImageURL, err = mapillary.GetImageURL(response.Key, 320)
+	return response, err
+}
+
+// Preview shows more information about a result.
+func (s *UbuntuMapillaryScope) Preview(result *scopes.Result, metadata *scopes.ActionMetadata, reply *scopes.PreviewReply, cancelled <-chan bool) error {
+	header := scopes.NewPreviewWidget("header", "header")
+	header.AddAttributeMapping("title", "title")
+	header.AddAttributeMapping("subtitle", "subtitle")
+
+	image := scopes.NewPreviewWidget("image", "image")
+	image.AddAttributeMapping("source", "art")
+
+	return reply.PushWidgets(header, image)
+}
+
+func main() {
+	if err := scopes.Run(&UbuntuMapillaryScope{}); err != nil {
+		log.Fatalln(err)
+	}
+}
